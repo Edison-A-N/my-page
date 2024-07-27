ebb94be97af954f44dabe75dba96120e1f24da28
nan.zhang <nan.zhang@xtalpi.com>
2024-07-27 16:22:00 +0800

    add about

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+    return (
+        <div>
+            <h1>关于我们</h1>
+            <p>这是关于我们的页面。这里可以放置一些内容。</p>
+        </div>
+    );
+};
+
+export default AboutPage;
